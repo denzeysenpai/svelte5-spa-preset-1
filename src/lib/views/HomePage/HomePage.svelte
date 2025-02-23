9a46e129c3308e5710c671a4d19341a9a39f92f3
Mark Solante <marksolante221@gmail.com>
2025-02-23 14:39:14 +0800

    [0.0.4 : Main Page Guide]

--- src/lib/views/HomePage/HomePage.svelte
+++ src/lib/views/HomePage/HomePage.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
 </script>
 
-<p>Home Page!</p>
+<main class="main-page-content">
+	<!-- LANDING PAGE SECTION -->
+	<!-- LANDING PAGE SECTION -->
+
+	<!-- SUMMARY OF ABOUT US OR INTRODUCTION -->
+	<!-- SUMMARY OF ABOUT US OR INTRODUCTION -->
+
+	<!-- VISUALS -->
+	<!-- VISUALS -->
+
+	<!-- CALL TO ACTION -->
+	<!-- CALL TO ACTION -->
+</main>
