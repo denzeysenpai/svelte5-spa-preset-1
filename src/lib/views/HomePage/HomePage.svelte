83315e1994db567a4a14896c2cba24330bdb452b
Mark Solante <marksolante221@gmail.com>
2025-02-23 16:26:10 +0800

    [0.0.6 : Landing Page Responsiveness]

--- src/lib/views/HomePage/HomePage.svelte
+++ src/lib/views/HomePage/HomePage.svelte
@@ -8,17 +8,25 @@
 <main class="main-page-content">
 	<!-- LANDING PAGE SECTION -->
 	<section class="landing-page">
-		<div class="floating-box-foo">
+		<div class="floating-box-foo" id="actions">
 			<div class="upper">
 				<h1>{businessName}</h1>
 				<p>{businessShortMessage}</p>
 			</div>
 			<div class="lower">
-				<button id="action-1">Action 1</button>
-				<button id="action-2">Action 2</button>
+				<button id="action-1">
+					<p>Action 1</p>
+					<span class="button-effect" id="mask"></span>
+					<span class="button-effect" id="bg"></span>
+				</button>
+				<button id="action-2">
+					<p>Action 2</p>
+					<span class="button-effect" id="mask"></span>
+					<span class="button-effect" id="bg"></span>
+				</button>
 			</div>
 		</div>
-		<div class="floating-box-foo">
+		<div class="floating-box-foo" id="logo">
 			<img src="" alt="logo element here" class="floating-logo" />
 		</div>
 		<img src={Assets.LandingPageBG} alt="" srcset="" class="landing-page-bg" />
